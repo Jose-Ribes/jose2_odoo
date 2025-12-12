c8075f3fb942bf22fe355c4acb446581bc8b8617
Jose <josribgin@alu.edu.gva.es>
2025-12-12 12:12:21 +0100

    Cambios en Tareas y Creación de Restaurante

--- data/addons/gestion_tareas_jose/models/models.py
+++ data/addons/gestion_tareas_jose/models/models.py
@@ -1,12 +1,48 @@
 from odoo import models, fields, api
 
 
-class gestion_tareas_jose(models.Model):
-     _name = 'gestion_tareas_jose.gestion_tareas_jose'
-     _description = 'gestion_tareas_jose.gestion_tareas_jose'
+class tareas_jose(models.Model):
+    _name = 'gestion_tareas_jose.tareas_jose'
+    _description = 'gestion_tareas_jose.tareas_jose'
 
-     nombre = fields.Char()
-     descripcion = fields.Text()
+    name = fields.Char(
+        string="Name", 
+        required=True, 
+        help="Introduzca el nombre de la tarea")
+
+    descripcion = fields.Text(
+        string="Descripción", 
+        help="Breve descripción de la tarea")
+
+    fecha_creacion = fields.Date(
+        string="Fecha Creación", 
+        required=True, 
+        help="Fecha en la que se dio de alta la tarea")
+
+    fecha_ini = fields.Datetime(
+        string="Fecha Inicio", 
+        required=True, 
+        help="Fecha y hora de inicio de la tarea")
+
+    fecha_fin = fields.Datetime(
+        string="Fecha Final", 
+        help="Fecha y hora de finalización de la tarea")
+
+    finalizado = fields.Boolean(
+        string="Finalizado", 
+        help="Indica si la tarea ha sido finalizada o no")
+    
+    sprint = fields.Many2one(
+        'gestion_tareas_jose.sprints_jose', 
+        string='Sprint relacionado', 
+        ondelete='set null', 
+        help='Sprint al que pertenece esta tarea')
+    rel_tecnologias = fields.Many2many(
+        comodel_name='gestion_tareas_jose.tecnologias_jose',
+        relation='relacion_tareas_tecnologias',
+        column1='rel_tareas',
+        column2='rel_tecnologias',
+        string='Tecnologías')
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #
@@ -15,3 +51,56 @@ class gestion_tareas_jose(models.Model):
 #         for record in self:
 #             record.value2 = float(record.value) / 100
 
+class sprints_jose(models.Model):
+    _name = 'gestion_tareas_jose.sprints_jose'
+    _description = 'gestion_tareas_jose.sprints_jose'
+    _rec_name = 'nombre'
+
+    nombre = fields.Char(
+        string="Nombre Sprint", 
+        required=True, 
+        help="Introduzca el nombre del sprint")
+
+    descripcion = fields.Text(
+        string="Descripción", 
+        help="Breve descripción del sprint")
+
+    fecha_ini = fields.Datetime(
+        string="Fecha Inicio", 
+        required=True, 
+        help="Fecha y hora de inicio de la tarea")
+
+    fecha_fin = fields.Datetime(
+        string="Fecha Final", 
+        help="Fecha y hora de finalización de la tarea")
+        
+    tareas = fields.One2many(
+        'gestion_tareas_jose.tareas_jose', 
+        'sprint', 
+        string='Tareas del Sprint')
+    
+class tecnologias_jose(models.Model):
+    _name = 'gestion_tareas_jose.tecnologias_jose'
+    _description = 'Modelo de Tecnologías'
+
+    name = fields.Char(
+        string="Nombre", 
+        required=True, 
+        help="Nombre de la tecnología")
+
+    descripcion = fields.Text(
+        string="Descripción", 
+        help="Breve descripción de la tecnología")
+
+    logo = fields.Image(
+        string="Logo", 
+        max_width=256, 
+        max_height=256,
+        help="Logo de la tecnología")
+    
+    rel_tareas = fields.Many2many(
+        comodel_name='gestion_tareas_jose.tareas_jose',
+        relation='relacion_tareas_tecnologias',
+        column1='rel_tecnologias',
+        column2='rel_tareas',
+        string='Tareas')
