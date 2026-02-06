108d7e6a550fc70528f98ec9362aeba933ed4819
Jose <josribgin@alu.edu.gva.es>
2026-02-06 12:53:14 +0100

    Contenido nuevo en el README.md y medio completo a falta de revisar el models.py y el fichero de security completo

--- data/addons/dronify_jose/models/models.py
+++ data/addons/dronify_jose/models/models.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 from odoo import models, fields, api
+from .logica_dronify import calcular_consumo_vuelo
 
 
 class dronify_jose(models.Model):
@@ -77,3 +80,125 @@ class drones_jose(models.Model):
         column1='dron_id',
         column2='contacto_id',
         string="Pilotos certificados para este dron")
+    
+class paquetes_jose(models.Model):
+    _name = 'dronify_jose.paquetes_jose'
+    _description = 'dronify_jose.paquetes_jose'
+
+    codigo = fields.Char(
+        string="Codigo",
+        help="Identificador único",
+        readonly=True,
+        default=lambda self: datetime.now().strftime("%Y%m%d%H%M%S")
+        )
+    
+    name = fields.Char(
+        string="Nombre", 
+        help="Descripción del contenido",
+        required=True
+        )
+    
+    peso = fields.Float(
+        string="Peso",
+        required=True,
+        help="Peso en kilogramos"
+    )
+
+    cliente_id = fields.Many2one(
+        'res.partner',
+        string='Cliente que envía el paquete',
+        required=True,
+        domain=[('es_cliente', '=', True)]
+    )
+
+    vuelo_id = fields.Many2one(
+        'dronify_jose.vuelos_jose',
+        string='Vuelo asignado',
+        readonly=True
+    )
+
+    dron_relacionado = fields.Char(
+        related='vuelo_id.dron_id.name',
+        string="Nombre del dron del vuelo",
+        readonly=True,
+        store=True
+    )
+
+class vuelos_jose(models.Model):
+    _name = 'dronify_jose.vuelos_jose'
+    _description = 'dronify_jose.vuelos_jose'
+
+    codigo = fields.Char(
+        string="Codigo",
+        help="Código único del vuelo",
+        readonly=True,
+        default=lambda self: datetime.now().strftime("%Y%m%d%H%M%S")
+        )
+    
+    name = fields.Char(
+        string="Nombre", 
+        help="Denominación de la misión",
+        required=True,
+        default=lambda self: datetime.now().strftime("%Y%m%d_Vuelo")
+        )
+
+    dron_id = fields.Many2one(
+        'dronify_jose.drones_jose',
+        string='Dron asignado',
+        required=True
+    )
+
+    piloto_id = fields.Many2one(
+        'res.partner',
+        string='Piloto responsable',
+        required=True,
+        domain=[('es_piloto', '=', True)]
+    )
+
+    paquetes_ids = fields.One2many(
+        'dronify_jose.paquetes_jose', 
+        'vuelo_id', 
+        string='Paquetes a transportar')
+    
+    preparado = fields.Boolean(
+        string="Indica si el vuelo está listo para ejecutarse"
+    )
+
+    realizado = fields.Boolean(
+        string="Indica si el vuelo se ha completado"
+    )
+
+    peso_total = fields.Float(
+        string="Peso Total",
+        compute="_compute_peso_total",
+        store=True,
+        help="Suma del peso de todos los paquetes asignados"
+    )
+
+    consumo_estimado = fields.Float(
+        string="Consumo Estimado",
+        compute="_compute_consumo_estimado",
+        store=True,
+        help="Porcentaje de batería que consumirá el vuelo"
+    )
+
+    # MÉTODOS @api.depends (COMPUTADOS) ******************************
+    # ***************************************************************
+
+    @api.depends('paquetes_ids.peso')
+    def _compute_peso_total(self):
+        for vuelo in self:
+            vuelo.peso_total = sum(vuelo.paquetes_ids.mapped('peso')) if vuelo.paquetes_ids else 0.0
+
+    @api.depends('peso_total', 'dron_id')
+    def _compute_consumo_estimado(self):
+        for vuelo in self:
+            # Obtener si el cliente es VIP
+            cliente_vip = False
+            if vuelo.paquetes_ids:
+                cliente_vip = any(paquete.cliente_id.es_vip for paquete in vuelo.paquetes_ids)
+            
+            vuelo.consumo_estimado = calcular_consumo_vuelo(vuelo.peso_total, es_vip=cliente_vip)
+
+    
+
